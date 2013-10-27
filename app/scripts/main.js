439705304b805c7aa95146e3b420a4720866daf0
Tobias Hartmann <hartmann.tobias@gmail.com>
2013-10-27 14:36:48 +0100

    weiteres für die basis struktur

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,12 @@ require.config({
     }
 });
 
-require(['views/login'], function(AppView) {
-    new AppView;
+require([  
+    'views/app',
+    'router',
+    'vm'
+], function(AppView, Router, Vm) {
+    var appView = Vm.create({}, 'AppView', AppView);
+    appView.render();
+    Router.initialize({appView: appView}); 
 });
---
+++ app/scripts/router.js
@@ -0,0 +1,54 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function ($, _, Backbone) {
+    
+  var AppRouter = Backbone.Router.extend({
+    initialize: function(options) {
+      this.appView = options.appView;
+    },
+ 
+    register: function (route, name, path) {
+      var self = this;
+ 
+      this.route(route, name, function () {
+        var args = arguments;
+ 
+        require([path], function (module) {
+          var options = null;
+          var parameters = route.match(/[:\*]\w+/g);
+ 
+          // Map the route parameters to options for the View.
+          if (parameters) {
+            options = {};
+            _.each(parameters, function(name, index) {
+              options[name.substring(1)] = args[index];
+            });
+          }
+ 
+          var page = Vm.create(self.appView, name, module, options);
+          page.render();
+        });
+      });
+    }
+  });
+ 
+  var initialize = function(options){
+    var router = new AppRouter(options);
+ 
+    // Default route goes first
+    //router.register('*actions', 'DashboardPage', 'views/dashboard/page');
+    //router.register('optimize', 'OptimizePage', 'views/optimize/page');
+    //router.register('modules', 'ModulesPage', 'views/modules/page');
+    //router.register('backbone', 'BackbonePage', 'views/backbone/page');
+    //router.register('backbone/:section', 'BackbonePage', 'views/backbone/page');
+    //router.register('manager', 'ManagerPage', 'views/manager/page');
+ 
+    Backbone.history.start();
+  };
+ 
+  return {
+    initialize: initialize
+  };
+});
